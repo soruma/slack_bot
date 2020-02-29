72b49a9fc61837adc82952a0dbf6abff5b69c60c
soruma <soruma.net@gmail.com>
2020-02-29 17:42:35 +0900

    Add echo server

--- lib/home_miku/web/router.ex
+++ lib/home_miku/web/router.ex
@@ -1,19 +1,36 @@
 defmodule HomeMiku.Web.Router do
   use Plug.Router
 
+  plug Plug.Parsers, parsers: [:urlencoded, :multipart]
+
   plug :match
   plug :dispatch
 
   get "/ping" do
     conn
     |> put_resp_content_type("application/json")
-    |> resp(:ok, Poison.encode!(%{message: "Pong!"}))
-    |> send_resp
+    |> send_resp(:ok, Poison.encode!(%{message: "Pong!"}))
+  end
+
+  get "/echo" do
+    conn
+    |> put_resp_content_type("application/json")
+    |> echo
+    |> send_resp(:ok, Poison.encode!(conn.params))
   end
 
   match _ do
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(:not_found, Poison.encode!(%{message: "Oops!"}))
+  end
+
+  def echo(conn) do
+    case Map.fetch(conn.params, "message") do
+      {:ok, message} -> Slack.Web.Chat.post_message("bot-sandbox", message, %{token: System.get_env("SLACK_BOT_API_TOKEN"), as_user: true})
+      _ -> {:ok}
+    end
+
+    conn
   end
 end
