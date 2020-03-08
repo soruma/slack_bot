7568d042f9ed7183dc746cddf3fec33b93e0738f
soruma <soruma.net@gmail.com>
2020-03-08 12:05:24 +0900

    Return :skip if nothing is done with react_to_message

--- lib/home_miku/slack/bot.ex
+++ lib/home_miku/slack/bot.ex
@@ -2,22 +2,22 @@ defmodule HomeMiku.Slack.Bot do
   import HomeMiku.Gettext
 
   @spec react_to_message(tuple, Slack.State.t) :: tuple
-  def react_to_message(%{type: "message", subtype: "message_changed"}, _), do: {:ok}
-  def react_to_message(%{type: "message", subtype: "message_deleted"}, _), do: {:ok}
-  def react_to_message(%{type: "message", subtype: "channel_join"}, _), do: {:ok}
+  def react_to_message(%{type: "message", subtype: "message_changed"}, _), do: {:skip}
+  def react_to_message(%{type: "message", subtype: "message_deleted"}, _), do: {:skip}
+  def react_to_message(%{type: "message", subtype: "channel_join"}, _), do: {:skip}
 
   def react_to_message(%{type: "message", attachments: attachments}, _), do: interpret_attachment(attachments)
   def react_to_message(message = %{type: "message"}, slack), do: interpret_message(message, slack)
 
   @spec interpret_attachment(list(tuple)) :: tuple
-  def interpret_attachment([]), do: {:ok}
+  def interpret_attachment([]), do: {:skip}
   def interpret_attachment([attachment | attachments]) do
     interpret_attachment(attachments)
 
     if attachment.title == "Approached at home" do
       {:send_message, gettext("Welcome back, master :heart:")}
     else
-      {:ok}
+      {:skip}
     end
   end
 
@@ -26,7 +26,7 @@ defmodule HomeMiku.Slack.Bot do
     if message_to_myself(message, slack) do
       {:send_message, gettext("I got a message!")}
     else
-      {:ok}
+      {:skip}
     end
   end
 
